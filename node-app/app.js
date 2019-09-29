e47228fe1a97825a5c0e0464b35d4f1832c2b312
unknown <andy0n7@gmail.com>
2019-09-29 13:48:01 +0300

    implemented mongoose ORD (broken package-lock.json)

--- node-app/app.js
+++ node-app/app.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 
 import { listen  } from './utilities/http/express'
-import { connect } from './utilities/databases/mongo'
+import { connect } from './utilities/databases/mongoose'
 import { configure } from './utilities/locale/i18n'
 
 configure()
