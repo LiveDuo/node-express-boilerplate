ab4978f2d3e8d447ab6e2c55d5e5d3bf4434633b
unknown <andy0n7@gmail.com>
2019-09-30 02:16:52 +0300

    implemented deleteUser and tests

---
+++ node-app/controllers/users/deleteUser.test.js
@@ -0,0 +1,31 @@
+import { HttpStatusCodes } from '../../utilities/http/httpStatusCodes'
+import request from 'supertest'
+
+import { app } from '../../utilities/http/express'
+
+describe('Delete users controller', () => {
+	it('should create and delete a new user', async () => {
+		const passwordRandom = Math.floor(Math.random()*(999-100+1)+100)
+		const emailId = Math.floor(Math.random()*(999-100+1)+100)
+
+		const mockUser = {
+			first_name: 'Name',
+			last_name: 'Surname',
+			password: `password${passwordRandom}`,
+			email: `${emailId}@gmail.com`,
+			gender: 'm',
+			language: 'Greek',
+			age: 25,
+			favorites: []
+		}
+
+		const res = await request(app).post('/users/create').send(mockUser).set('Accept', 'application/json')
+
+		expect(res.statusCode).toEqual(HttpStatusCodes.OK)
+
+		let token = res.body.jwt
+		const res2 = await request(app).post('/users/delete').set('Accept', 'application/json').set('Authorization', `Bearer ${token}`)
+
+		expect(res2.statusCode).toEqual(HttpStatusCodes.OK)
+	})
+})
--- node-app/controllers/users/router.js
+++ node-app/controllers/users/router.js
@@ -1,7 +1,12 @@
+import { verifyToken } from '../../services/authentication/verify'
 import { createUser } from '../../controllers/users/createUser'
+import { deleteUser } from '../../controllers/users/deleteUser'
+
 import express from 'express'
+
 const router = express.Router()
 
 router.post('/create', createUser)
+router.post('/delete', verifyToken, deleteUser)
 
 export { router }
