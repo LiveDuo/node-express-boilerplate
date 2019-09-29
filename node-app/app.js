f1d4bc862db82cf11eae33d5a8c51b19f440d075
unknown <andy0n7@gmail.com>
2019-09-29 13:39:25 +0300

    setup i18n (broken package-lock.json)

--- node-app/app.js
+++ node-app/app.js
@@ -2,5 +2,8 @@ import 'dotenv/config';
 
 import { listen  } from './utilities/http/express'
 import { connect } from './utilities/databases/mongo'
+import { configure } from './utilities/locale/i18n'
+
+configure()
 
 connect().then(listen)
--- node-app/utilities/http/express.js
+++ node-app/utilities/http/express.js
@@ -3,12 +3,13 @@ import express from 'express'
 
 import { logger } from './morgan'
 import { parser, json } from './bodyParser'
-import { bodyParser } from './bodyParser'
+import { locale } from '../locale/i18n'
 import { config } from '../../config/config'
 
 const app = express()
 
 app.use(logger)
+app.use(locale)
 app.use(parser)
 app.use(json)
 
