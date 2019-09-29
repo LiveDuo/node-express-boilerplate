b8254145bd38b9a5fd0b1c68f2a8825613b814d3
unknown <andy0n7@gmail.com>
2019-09-29 18:38:53 +0300

    cleared up createUsers controller

--- node-app/controllers/users/createUser.js
+++ node-app/controllers/users/createUser.js
@@ -5,21 +5,7 @@ import { hash } from '../../utilities/authentication/bcrypt'
 
 const createUser = async (req, res) => {
     
-    // const passwordHashed = await hash('password', 10)
-
-    // const emailId = Math.floor(Math.random()*(999-100+1)+100)
-
-    // const mockUser = {
-    //     first_name: 'Name',
-    //     last_name: 'Surname',
-    //     passwordHashed: passwordHashed,
-    //     email: `${emailId}@gmail.com`,
-    //     gender: 'm',
-    //     language: 'Greek',
-    //     age: 25,
-    //     favorites: []
-    // }
-
+    // BUG no password error catch
     const passwordHashed = await hash(req.body.password, 10)
 
     const user = {
@@ -36,7 +22,6 @@ const createUser = async (req, res) => {
     try {
         const result = await User.save()
         const token = sign({ id: result._id }, process.env.JWT_KEY)
-        // console.log(result)
         return res.status(HttpStatusCodes.OK).send({ jwt: token })
     } catch (error) {
         console.log(error.message)
