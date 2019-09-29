0f901a97f48352a1e7c3c7e91a2acc078d2e2603
unknown <andy0n7@gmail.com>
2019-09-30 02:33:02 +0300

    fix missing password in request bug

--- node-app/controllers/users/createUser.js
+++ node-app/controllers/users/createUser.js
@@ -5,20 +5,25 @@ import { hash } from '../../utilities/authentication/bcrypt'
 
 const createUser = async (req, res) => {
 
-	// BUG no password error catch
-	const passwordHashed = await hash(req.body.password, 10)
-
 	const user = {
 		first_name: req.body.first_name,
 		last_name: req.body.last_name,
-		password: passwordHashed,
 		email: req.body.email,
 		gender: req.body.gender,
 		language: req.body.language,
 		age: req.body.age,
 		favorites: req.body.favorites
 	}
+
+	try {
+		user.password = await hash(req.body.password, 10)
+	} catch (error) {
+		console.log(error.message)
+		return res.status(HttpStatusCodes.BAD_REQUEST).send({ message: res.__('responses').invalid_request})
+	}
+
 	const User = new UserModel(user)
+	
 	try {
 		const result = await User.save()
 		const token = sign({ id: result._id }, process.env.JWT_KEY)
