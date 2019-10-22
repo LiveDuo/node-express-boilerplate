8a25cfbc63b38904354e550058584582b33994a1
unknown <andy0n7@gmail.com>
2019-10-22 14:40:03 +0300

    remove unused field from schema

--- node-app/models/User/user.js
+++ node-app/models/User/user.js
@@ -8,13 +8,7 @@ const UserSchema = new Schema({
 	last_name: { type: String, required: true, max: 50 },
 	password: { type: String, required: true },
 	email: EmailSchema,
-	gender: { type: String, max: 50 },
-	language: { type: String, max: 50 },
-	age: {type: Number, min: 0, max: 150},
-	city: { type: String, max: 50 },
-	state: { type: String, max: 50 },
-	type: { type: String, max: 50 },
-	favorites: [{ type: String }]
+	image_urls: [{ type: String }]
 })
 
 export const UserModel = mongoose.model('User', UserSchema)
