3947ba0c806dac943de115c0a806110b41fe96be
unknown <andy0n7@gmail.com>
2019-10-22 14:38:46 +0300

    implemented websockets

--- node-app/app.js
+++ node-app/app.js
@@ -1,8 +1,9 @@
 import 'dotenv/config'
 
-import { listen  } from './services/http/express'
+// import { listen  } from './services/http/express'
 import { connect } from './services/databases/mongoose'
 import { configure } from './services/locale/i18n'
+import { listen } from './services/websockets/ws'
 
 configure()
 
