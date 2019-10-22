375d35c1e99ee412d228ab16773c6d31bcf0d902
unknown <andy0n7@gmail.com>
2019-10-22 14:41:11 +0300

    revise remove password from request method

--- node-app/controllers/users/getUser.js
+++ node-app/controllers/users/getUser.js
@@ -6,12 +6,11 @@ import { getCachedThenQuery } from '../../services/caching/nodeCache'
 const getUser = async (req, res) => {
 
 	const userId = req.params.userId
-	const promise = UserModel.findById(userId)
+	const promise = UserModel.findById(userId).select({ password: 0 })
 
 	try {
 		const result = await getCachedThenQuery('get-user-id-'-userId, promise)
 		if (result) {
-			result.password = undefined // maybe a bad idea
 			return res.status(HttpStatusCodes.OK).send(result)
 		} else {
 			return res.status(HttpStatusCodes.BAD_REQUEST).send({message: res.__('responses').something_went_wrong})
